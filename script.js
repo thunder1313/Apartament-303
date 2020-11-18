b3bbbf659bd0d398e0caa74b8ecd92ea80e79fce
Maksym <maksympata@maksyms-air.home>
2020-11-18 20:37:32 +0100

    Fix bug with logo not returning to its right position after opening sidebar and returning to width over 800px

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
 const logo = document.querySelector('#logo');
 
+// const windowWidth = document.querySelector('#width');
+
 const hamburgerClick = () => {
     hamburger.classList.toggle('hamburger--active');
     sidebar.classList.toggle('sidebar--active');
     logo.classList.toggle('logo--active');
 }
 
+const windowResized = () =>{
+    if(window.innerWidth > 800){
+        hamburger.classList.remove('hamburger--active');
+        sidebar.classList.remove('sidebar--active');
+        logo.classList.remove('logo--active');
+    }
+}
+
 hamburger.addEventListener('click', hamburgerClick);
+window.addEventListener('resize', windowResized)
