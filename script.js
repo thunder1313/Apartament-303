67087187716856b5a60bd2e2a3ee10a3dcfa980b
Maksym <maksympata@maksyms-air.home>
2020-11-18 19:59:15 +0100

    Add responsive navbar, hamburger menu and sidebar

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
+const logo = document.querySelector('#logo');
 
 const hamburgerClick = () => {
     hamburger.classList.toggle('hamburger--active');
+    sidebar.classList.toggle('sidebar--active');
+    logo.classList.toggle('logo--active');
 }
 
 hamburger.addEventListener('click', hamburgerClick);
